dad5fd1d67ab664909097ba82913a56b475d361d
Viktor Tomilin <viktortomilin@gmail.com>
2019-10-09 00:47:48 +0300

    Make it alive

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # decaffeinator
-coffeescript to es6 converter
+> No more tears, no more pain, no more coffeescript...
+
+## Features
+
+This tool can convert your CoffeeScript file to es6 and will convert markup based on React DOM fabricator to JSX. No routine. Save your time and be happy.
+
+## Installation
+
+Just do the following bunch of commands:
+
+```sh
+git clone git@github.com:victortomilin/decaffeinator.git
+cd decaffeinator
+sh install
+```
+
+The script will automatically set up dependencies and add an own bin folder to the `PATH` variable to your `.bashrc` or `.zshrc` files to make you able to use "decoffeinator" as a command in your terminal.
+
+## How to use
+
+If you are ready and have a target to get rid of coffee in some file then just type the following command in your terminal:
+
+```sh
+grind <file_name>
+```
+
+For example:
+
+```sh
+grind UglyComponent.coffee
+```
+
+In the end, you should get a new one file with the name `UglyComponent.js`. 
+
+## Requirements
+
+You need to have:
+- git
+- nodejs
+- npm
---
+++ src/dom-factories-calls-to-calls-with-react-dom-prefix.js
@@ -0,0 +1,191 @@
+'use strict';
+
+export default (file, api, options) => {
+  const j = api.jscodeshift;
+  const root = j(file.source);
+
+  let hasModifications;
+
+  const domFactories = {
+    a: 'a',
+    abbr: 'abbr',
+    address: 'address',
+    area: 'area',
+    article: 'article',
+    aside: 'aside',
+    audio: 'audio',
+    b: 'b',
+    base: 'base',
+    bdi: 'bdi',
+    bdo: 'bdo',
+    big: 'big',
+    blockquote: 'blockquote',
+    body: 'body',
+    br: 'br',
+    button: 'button',
+    canvas: 'canvas',
+    caption: 'caption',
+    cite: 'cite',
+    code: 'code',
+    col: 'col',
+    colgroup: 'colgroup',
+    data: 'data',
+    datalist: 'datalist',
+    dd: 'dd',
+    del: 'del',
+    details: 'details',
+    dfn: 'dfn',
+    dialog: 'dialog',
+    div: 'div',
+    dl: 'dl',
+    dt: 'dt',
+    em: 'em',
+    embed: 'embed',
+    fieldset: 'fieldset',
+    figcaption: 'figcaption',
+    figure: 'figure',
+    footer: 'footer',
+    form: 'form',
+    h1: 'h1',
+    h2: 'h2',
+    h3: 'h3',
+    h4: 'h4',
+    h5: 'h5',
+    h6: 'h6',
+    head: 'head',
+    header: 'header',
+    hgroup: 'hgroup',
+    hr: 'hr',
+    html: 'html',
+    i: 'i',
+    iframe: 'iframe',
+    img: 'img',
+    input: 'input',
+    ins: 'ins',
+    kbd: 'kbd',
+    keygen: 'keygen',
+    label: 'label',
+    legend: 'legend',
+    li: 'li',
+    link: 'link',
+    main: 'main',
+    map: 'map',
+    mark: 'mark',
+    menu: 'menu',
+    menuitem: 'menuitem',
+    meta: 'meta',
+    meter: 'meter',
+    nav: 'nav',
+    noscript: 'noscript',
+    object: 'object',
+    ol: 'ol',
+    optgroup: 'optgroup',
+    option: 'option',
+    output: 'output',
+    p: 'p',
+    param: 'param',
+    picture: 'picture',
+    pre: 'pre',
+    progress: 'progress',
+    q: 'q',
+    rp: 'rp',
+    rt: 'rt',
+    ruby: 'ruby',
+    s: 's',
+    samp: 'samp',
+    script: 'script',
+    section: 'section',
+    select: 'select',
+    small: 'small',
+    source: 'source',
+    span: 'span',
+    strong: 'strong',
+    style: 'style',
+    sub: 'sub',
+    summary: 'summary',
+    sup: 'sup',
+    table: 'table',
+    tbody: 'tbody',
+    td: 'td',
+    textarea: 'textarea',
+    tfoot: 'tfoot',
+    th: 'th',
+    thead: 'thead',
+    time: 'time',
+    title: 'title',
+    tr: 'tr',
+    track: 'track',
+    u: 'u',
+    ul: 'ul',
+    'var': 'var',
+    video: 'video',
+    wbr: 'wbr',
+
+    // SVG
+    circle: 'circle',
+    clipPath: 'clipPath',
+    defs: 'defs',
+    ellipse: 'ellipse',
+    g: 'g',
+    image: 'image',
+    line: 'line',
+    linearGradient: 'linearGradient',
+    mask: 'mask',
+    path: 'path',
+    pattern: 'pattern',
+    polygon: 'polygon',
+    polyline: 'polyline',
+    radialGradient: 'radialGradient',
+    rect: 'rect',
+    stop: 'stop',
+    svg: 'svg',
+    text: 'text',
+    tspan: 'tspan'
+
+  };
+
+  const isDomFactory = (name) => {
+    return domFactories.hasOwnProperty(name);
+  };
+
+  const removeNode = (path) => {
+    j(path).remove();
+  };
+
+  function replaceReactDOMReferences(j, root) {
+    let hasModifications = false;
+
+    // Replace calls like `div(..)` with `React.DOM.div(...)` so further transforms can be used
+    root
+      .find(j.Identifier)
+      .filter((path) => {
+        return path.name === 'callee' && isDomFactory(path.node.name)
+      })
+      .forEach(path => {
+        hasModifications = true;
+        j(path).replaceWith(j.identifier('React.DOM.' + path.node.name));
+      });
+
+    // Remove React.DOM imports and variables (e.g. `import {div} from React.DOM;`)
+    // Not that this doesn't cover all types of imports
+    root
+      .find(j.MemberExpression)
+      .filter((path) => {
+        return  path.node.object.name === 'React'
+          && path.node.property.name === 'DOM'
+          && path.parent.node.type === 'VariableDeclarator';
+      })
+      .forEach(path => {
+        hasModifications = true;
+        removeNode(path.parent.parent);
+      });
+
+    return hasModifications;
+  }
+
+  hasModifications = replaceReactDOMReferences(j, root);
+
+  return hasModifications
+    ? root.toSource({ quote: 'single' })
+    : null;
+};
